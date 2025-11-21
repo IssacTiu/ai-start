b27b0de5b6a9fa95a965a22f4f87d83c79a08242
IssacTiu <issac.tiu@uleth.ca>
2025-11-21 13:37:45 -0700

    Added Javascript files

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log("JavaScript is working!");
+
+$("body").css("background-color", "#dbdae2");
+
+$(".title h1").html("Abundant Memory, Relational Intelligences")
+$(".title").append("<p>October 17th, 2025 - January 23rd, 2026</p>")
+
+$(".funding h3").append("<p>This exhibition draws upon research supported by the Government of Canada’s New Frontiers in Research Fund (NFRF) and the Social Sciences and Humanities Research Council, and the Canada Foundation for Innovation.</p>")
+
+$(".logos").html("<img src='assets/ulag_logo_black.webp'>")
